789ca38441d164b5e9053374e4bf501d4f2e0b1b
Bobby Brice <bobby.brice@gmail.com>
2020-01-11 20:46:27 -0500

    added stateless components and downloaded pokeClasses to an object

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import PokeList from './PokeList';
 import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
   render() {
     return (
       <div className="App">
-        hellohello
+        <PokeList />
       </div>
     );
   }
