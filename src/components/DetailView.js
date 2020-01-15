79c1590412ee32357878f57b69beaa35b9c4f0fb
Bobby Brice <bobby.brice@gmail.com>
2020-01-14 19:55:00 -0500

    added custom poke font

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import text from '../assets/pokeFont.png';
 import './styles/DetailView.css';
 
 const DetailView = ({ pokemon }) => {
@@ -6,7 +7,7 @@ const DetailView = ({ pokemon }) => {
 
   return (
     <section className="detail-view">
-      <h1 className="header">Bobby's Pokedex</h1>
+      <img src={text} alt="custom text"/>
       <img src={sprite}className="sprite-image" alt="sprite"/>
       <div className="data-wrapper">
         <h1 className="data-name">ID: {name}</h1>
