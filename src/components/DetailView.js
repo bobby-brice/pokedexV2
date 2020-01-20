0190ddea99cb0e0a86ce4a518fe8eb01ad5316a2
Bobby Brice <bobby.brice@gmail.com>
2020-01-20 17:20:37 -0500

    bug fix

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -4,11 +4,11 @@ import './styles/DetailView.css';
 
 const DetailView = ({ pokemon }) => {
   const { name, sprite, type, exp, weight} = pokemon; //destructure our data from the pokemon object
-
+  
   return (
     <section className="detail-view">
       <img src={text} alt="custom text"/>
-      <img src={sprite}className="sprite-image" alt="sprite"/>
+      <img src={sprite} className="sprite-image" alt="sprite"/>
       <div className="data-wrapper">
         <h1 className="data-name">ID: {name}</h1>
         <p className="data-char">Type: {type}</p>
